9ca98f2e938525bd2231f23b5b08c3e639738f66
Aditya_Pathak <buddingwebdeveloperind@gmail.com>
2019-09-26 07:50:47 +0530

    adding checkout summary component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import Layout from './hoc/Layout/Layout';
 
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
+import Checkout from './containers/Checkout/Checkout';
+
 class App extends Component {
   render(){
     return (
       <div>
         <Layout>
           <BurgerBuilder />
+          <Checkout />
         </Layout>
       </div>
   );
