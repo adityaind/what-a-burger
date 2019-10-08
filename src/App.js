0cb7517c9f3fed8f153d3ee43ce80327031a286f
Aditya_Pathak <buddingwebdeveloperind@gmail.com>
2019-10-08 15:14:40 +0530

    adding checkout page and displaying orders

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 import Checkout from './containers/Checkout/Checkout';
-
+import Orders from './containers/Orders/Orders';
 class App extends Component {
   render(){
     return (
@@ -13,6 +13,7 @@ class App extends Component {
         <Layout>
         	<Switch>
 	         <Route path="/checkout" component={Checkout} />
+           <Route path="/orders" component={Orders} />
 	         <Route path="/" exact component={BurgerBuilder} />
         	</Switch>
         </Layout>
