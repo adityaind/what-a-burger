91aa42f1f42b0d3f969caa3d7e43744ff8e3568e
Aditya <buddingwebdeveloperind@gmail.com>
2019-10-16 18:45:00 +0530

    authentication using firebase

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+//https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDC9W0t-5C6A_Z9cGCtCc5Pg5ac9zdKwrM
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -6,10 +8,11 @@ export const authStart = () => {
     };
 };
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token, userId) => {
     return {
         type: actionTypes.AUTH_SUCCESS,
-        authData: authData
+        idToken: token,
+        userId: userId
     };
 };
 
@@ -20,8 +23,26 @@ export const authFail = (error) => {
     };
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDC9W0t-5C6A_Z9cGCtCc5Pg5ac9zdKwrM';
+        if (!isSignup) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDC9W0t-5C6A_Z9cGCtCc5Pg5ac9zdKwrM';
+        }
+        axios.post(url, authData)
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response.data.idToken, response.data.localId));
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail(err));
+            });
     };
 };
